5c18d3ec64b4ca3e81f2323afb980e17988daad2
Martin Kraft <martin@upspin.org>
2019-04-30 12:40:30 -0400

    MM-14410: WIP

--- components/team_groups_manage_modal/team_groups_manage_modal.jsx
+++ components/team_groups_manage_modal/team_groups_manage_modal.jsx
@@ -17,81 +17,91 @@ export default class TeamGroupsManageModal extends React.PureComponent {
         actions: PropTypes.shape({
             getGroupsAssociatedToTeam: PropTypes.func.isRequired,
             unlinkGroupSyncable: PropTypes.func.isRequired,
+            closeModal: PropTypes.func.isRequired,
         }).isRequired,
-    }
+    };
+
+    initialItems = async () => {
+        const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, '', 0, DEFAULT_NUM_PER_PAGE);
+        return {
+            items: data.groups,
+            totalCount: data.totalGroupCount,
+        };
+    };
+
+    renderRow = (item, component) => {
+        return (
+            <React.Fragment key={item.id}>
+                <img
+                    className='more-modal__image'
+                    src='/static/files/73209f482a967f9379602dc6253cf768.png'
+                    alt='group picture'
+                    width='32'
+                    height='32'
+                />
+                <div className='more-modal__details'>
+                    <div className='more-modal__name'>{item.display_name} - <span>
+                        <FormattedMessage
+                            id='numMembers'
+                            defaultMessage='{num, number} {num, plural, one {member} other {members}}'
+                            values={{
+                                num: item.member_count,
+                            }}
+                        /></span>
+                    </div>
+                </div>
+                <div className='more-modal__actions'>
+                    <button
+                        id='removeMember'
+                        type='button'
+                        className='btn btn-danger btn-message'
+                        onClick={() => this.props.actions.unlinkGroupSyncable(item.id, this.props.team.id, Groups.SYNCABLE_TYPE_TEAM).then(async () => {
+                            component.setState({loading: true});
+                            const {items} = await component.props.onPageChange(component.setState.page, component.state.searchTerm);
+                            component.setState({loading: false, items});
+                        })}
+                    >
+                        <FormattedMessage
+                            id='group_list_modal.removeGroupButton'
+                            defaultMessage='Remove Group'
+                        />
+                    </button>
+                </div>
+            </React.Fragment>
+        );
+    };
+
+    onPageChange = async (pageNumber, searchTerm) => {
+        const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, searchTerm, pageNumber, DEFAULT_NUM_PER_PAGE);
+        return {
+            items: data.groups,
+            totalCount: data.totalGroupCount,
+        };
+    };
+
+    onSearchInput = async (searchTerm) => {
+        const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, searchTerm, 0, DEFAULT_NUM_PER_PAGE);
+        return {
+            items: data.groups,
+            totalCount: data.totalGroupCount,
+        };
+    };
+
+    onHide = () => {
+        this.props.actions.closeModal(ModalIdentifiers.MANAGE_TEAM_GROUPS);
+    };
 
     render() {
         return (
             <ListModal
                 titleText={`${this.props.team.display_name} Groups`}
                 searchPlaceholderText='Search groups'
-                initialItems={async () => {
-                    const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, '', 0, DEFAULT_NUM_PER_PAGE);
-                    return {
-                        items: data.groups,
-                        totalCount: data.totalGroupCount,
-                    };
-                }}
-                renderRow={(item, component) => {
-                    return (
-                        <React.Fragment key={item.id}>
-                            <img
-                                className='more-modal__image'
-                                src='/static/files/73209f482a967f9379602dc6253cf768.png'
-                                alt='group picture'
-                                width='32'
-                                height='32'
-                            />
-                            <div className='more-modal__details'>
-                                <div className='more-modal__name'>{item.display_name} - <span>
-                                    <FormattedMessage
-                                        id='numMembers'
-                                        defaultMessage='{num, number} {num, plural, one {member} other {members}}'
-                                        values={{
-                                            num: item.member_count,
-                                        }}
-                                    /></span>
-                                </div>
-                            </div>
-                            <div className='more-modal__actions'>
-                                <button
-                                    id='removeMember'
-                                    type='button'
-                                    className='btn btn-danger btn-message'
-                                    onClick={() => this.props.actions.unlinkGroupSyncable(item.id, this.props.team.id, Groups.SYNCABLE_TYPE_TEAM).then(async () => {
-                                        component.setState({loading: true});
-                                        const {items} = await component.props.onPageChange(component.setState.page, component.state.searchTerm);
-                                        component.setState({loading: false, items});
-                                    })}
-                                >
-                                    <FormattedMessage
-                                        id='group_list_modal.removeGroupButton'
-                                        defaultMessage='Remove Group'
-                                    />
-                                </button>
-                            </div>
-                        </React.Fragment>
-                    );
-                }}
-                onPageChange={async (pageNumber, searchTerm) => {
-                    const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, searchTerm, pageNumber, DEFAULT_NUM_PER_PAGE);
-                    return {
-                        items: data.groups,
-                        totalCount: data.totalGroupCount,
-                    };
-                }}
+                initialItems={this.initialItems}
+                renderRow={this.renderRow}
+                onPageChange={this.onPageChange}
                 itemKey='id'
-                onSearchInput={async (searchTerm) => {
-                    const {data} = await this.props.actions.getGroupsAssociatedToTeam(this.props.team.id, searchTerm, 0, DEFAULT_NUM_PER_PAGE);
-                    return {
-                        items: data.groups,
-                        totalCount: data.totalGroupCount,
-                    };
-                }}
-                onHide={() => {
-                    this.props.actions.closeModal(ModalIdentifiers.MANAGE_TEAM_GROUPS)
-                    ;
-                }}
+                onSearchInput={this.onSearchInput}
+                onHide={this.onHide}
             />
         );
     }
